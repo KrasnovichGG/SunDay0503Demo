3570f3ae3497805232b3f3ec1385de1002bb313c
Alexsunder <Sasha-kr90@bk.ru>
2023-03-05 14:33:21 +0300

    AddWin

---
+++ SunDay0503Demo/CRUDO/AddWin.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using SunDay0503Demo.DB;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SunDay0503Demo.CRUDO
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWin.xaml
+    /// </summary>
+    public partial class AddWin : Window
+    {
+        public AddWin()
+        {
+            InitializeComponent();
+            CmbRroduct.ItemsSource = App.DBLopushok.ProductType.ToList();
+        }
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        private void BtnSelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            openFileDialog.Filter = "Image files|*.bmp;*.jpg;*.png|All files|*.*";
+            openFileDialog.FilterIndex = 1;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                File.ReadAllBytes(openFileDialog.FileName);
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.UriSource = new Uri(openFileDialog.FileName);
+                image.EndInit();
+                ImageBym.Source = image;
+            }
+        }
+
+        private void BtnAddToDB_Click(object sender, RoutedEventArgs e)
+        {
+            if (TxArticle.Text == "" || TxDes.Text == "" || TxMinCostAgent.Text == ""
+               || TxProdPers.Text == "" || TxProdShop.Text == "" || TtxBoxTitle.Text == "" || CmbRroduct.SelectedIndex == -1)
+            {
+                MessageBox.Show("Не оставляйте незаполенные поля, или же информация не сохранится", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            Product product = new Product();
+            {
+                product.Image = openFileDialog.FileName;
+                product.Title = TtxBoxTitle.Text;
+                product.ArticleNumber = TxArticle.Text;
+                product.MinCostForAgent = Convert.ToDecimal(TxMinCostAgent.Text);
+                product.ProductionPersonCount = Convert.ToInt32(TxProdPers.Text);
+                product.ProductionWorkshopNumber = Convert.ToInt32(TxProdShop.Text);
+                var idType = CmbRroduct.SelectedItem;
+                product.ProductTypeID = ((ProductType)idType).ID;
+            }
+            App.DBLopushok.Product.Add(product); 
+            App.DBLopushok.SaveChanges();
+            MessageBox.Show("Добавлено");
+            this.Close();
+        }
+
+        private void TxArticle_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                MessageBox.Show("Вводить только цифры!,допустимое значение 6 цифр", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true;
+            }
+        }
+
+        private void TxMinCostAgent_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                MessageBox.Show("Вводить только цифры!,допустимое значение 3 цифры", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true;
+            }
+        }
+
+        private void TxProdShop_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0))
+            {
+                MessageBox.Show("Вводить только цифры!,допустимое значение 2 цифры", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true;
+            }
+        }
+    }
+}
--- SunDay0503Demo/CRUDO/Main.xaml.cs
+++ SunDay0503Demo/CRUDO/Main.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SunDay0503Demo.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,58 @@ namespace SunDay0503Demo
         {
             InitializeComponent();
             LstItems.ItemsSource = App.DBLopushok.Product.ToList();
+            FeelCombobox();
+            CmbBoxFiltr.ItemsSource = App.DBLopushok.ProductType.ToList();
+        }
+        private void Sort()
+        {
+            switch (CmbBoxSort.SelectedItem)
+            { 
+                case "От А до Я":
+                LstItems.ItemsSource = null;
+                LstItems.ItemsSource = App.DBLopushok.Product.OrderBy(x => x.Title).ToList();
+                break;
+                case "От Я до А":
+                LstItems.ItemsSource = null;
+                LstItems.ItemsSource = App.DBLopushok.Product.OrderByDescending(x => x.Title).ToList();
+                break;
+                default:
+                LstItems.ItemsSource = null;
+                LstItems.ItemsSource = App.DBLopushok.Product.ToList();
+                break;
+
+            }
+        }
+        private void FeelCombobox()
+        {
+            CmbBoxSort.Items.Add("От А до Я");
+            CmbBoxSort.Items.Add("От Я до А");
+        }
+        private void TxPoisk_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var poisk = App.DBLopushok.Product.ToList();
+            poisk = poisk.Where(x => x.Title.ToLower().Contains(TxPoisk.Text.ToLower())).ToList();
+            LstItems.ItemsSource = poisk.ToList();
+        }
+
+        private void CmbBoxSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Sort();
+        }
+
+        private void BtnX_Click(object sender, RoutedEventArgs e)
+        {
+            //CmbBoxFiltr.SelectedIndex = -1;
+            CmbBoxSort.SelectedIndex = -1;
+            TxPoisk.Text = " ";
+            LstItems.ItemsSource = App.DBLopushok.Product.ToList();
+        }
+
+        private void CmbBoxFiltr_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedType = CmbBoxFiltr.SelectedItem;
+            var typeProduct = ((ProductType)selectedType).ID;
+            LstItems.ItemsSource = App.DBLopushok.Product.Where(x => x.ProductTypeID == typeProduct).ToList();
         }
     }
 }
